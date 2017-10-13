b2ca9b292a423c62ca74991ac530f4dd3bef0116
martsberger <bmarts@procuredhealth.com>
2017-10-13 10:47:32 -0400

    Add setup.py and travis.yml files

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-joinfield',
+    version='0.1',
+    description='A field type for Django models that allows joins to a related model without a foreign key.',
+    url='https://github.com/martsberger/django-joinfield',
+    download_url='https://github.com/martsberger/django-joinfield/archive/0.1.tar.gz',
+    author='Brad Martsberger',
+    author_email='bmarts@procuredhealth.com',
+    license='MIT',
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ],
+    packages=find_packages(),
+    install_requires=['django>=1.10']
+)
