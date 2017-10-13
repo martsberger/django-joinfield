a9f1c08281908c87dda0b0c9c54032c598eaa7ef
martsberger <bmarts@procuredhealth.com>
2017-10-13 10:59:24 -0400

    Import Joinfield correctly in the test suite

--- joinfield/tests/models.py
+++ joinfield/tests/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from joinfield.joinfield import JoinField
+
 
 class Surname(models.Model):
     name = models.CharField(primary_key=True, max_length=75)
@@ -7,4 +9,4 @@ class Surname(models.Model):
 
 class Person(models.Model):
     first_name = models.CharField(max_length=75)
-    last_name = models.JoinField(Surname, on_delete=models.DO_NOTHING, name='last_name')
+    last_name = JoinField(Surname, on_delete=models.DO_NOTHING, name='last_name')
