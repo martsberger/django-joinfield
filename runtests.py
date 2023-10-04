0a998f2823e54f344ef5b308d8f89cb4a96842c3
martsberger <bradley.marts@gmail.com>
2023-10-04 13:44:06 -0400

    simplify test runner

--- runtests.py
+++ runtests.py
@@ -5,21 +5,11 @@ from optparse import OptionParser
 
 def parse_args():
     parser = OptionParser()
-    parser.add_option('-s', '--settings', help='Define settings.')
-    parser.add_option('-t', '--unittest', help='Define which test to run. Default all.')
-    options, args = parser.parse_args()
-
-    if not options.settings:
-        parser.print_help()
-        sys.exit(1)
-
-    if not options.unittest:
-        options.unittest = ['joinfield']
-
-    return options
+    parser.add_option('-s', '--settings', default='joinfield.tests.test_sqlite_settings', help='Define settings.')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    options = parse_args()
+    options, tests = parse_args()
     os.environ['DJANGO_SETTINGS_MODULE'] = options.settings
 
     # Local imports because DJANGO_SETTINGS_MODULE needs to be set first
@@ -32,4 +22,4 @@ if __name__ == '__main__':
 
     TestRunner = get_runner(settings)
     runner = TestRunner(verbosity=2, interactive=True, failfast=False)
-    sys.exit(runner.run_tests([]))
+    sys.exit(runner.run_tests(tests))
